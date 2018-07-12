0776fef6a0db21c309f9fffb369a0002f4687418
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-07-12 23:59:06 +0900

    UPDATE // location info.plist

--- Practice_Swift/Practice_Swift/ViewController/MyLocationViewController.swift
+++ Practice_Swift/Practice_Swift/ViewController/MyLocationViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MyLocationViewController: UIViewController {
-
+class MyLocationViewController: UIViewController , CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+
 
 }
