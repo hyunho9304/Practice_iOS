946ce757c20b479f1608d6040aab67013be6481d
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-07-13 00:47:40 +0900

    UPDATE // current location mapView

--- Practice_Swift/Practice_Swift/ViewController/MyLocationViewController.swift
+++ Practice_Swift/Practice_Swift/ViewController/MyLocationViewController.swift
@@ -5,15 +5,17 @@
 //  Created by 박현호 on 2018. 7. 12..
 //  Copyright © 2018년 박현호. All rights reserved.
 //
-
+/*
 import UIKit
 import MapKit
 import CoreLocation
 
-class MyLocationViewController: UIViewController , CLLocationManagerDelegate {
+class MyLocationViewController: UIViewController , CLLocationManagerDelegate , MKMapViewDelegate {
     
     let locationManager = CLLocationManager()
     
+    @IBOutlet weak var currentLatitude: UILabel!
+    @IBOutlet weak var currentLongitude: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,6 @@ class MyLocationViewController: UIViewController , CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
         
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -39,5 +40,106 @@ class MyLocationViewController: UIViewController , CLLocationManagerDelegate {
         print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
 
-
 }
+
+*/
+/*
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class MyLocationViewController: UIViewController , CLLocationManagerDelegate , MKMapViewDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var currentLatitude: UILabel!
+    @IBOutlet weak var currentLongitude: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
+}
+*/
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class MyLocationViewController: UIViewController , CLLocationManagerDelegate , MKMapViewDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var currentLatitude: UILabel!
+    @IBOutlet weak var currentLongitude: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenter(coor, animated: true)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        mapView.mapType = MKMapType.standard
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "내 위치"
+        //annotation.subtitle = "현재위치"
+        mapView.addAnnotation(annotation)
+        
+        //centerMap(locValue)
+    }
+    
+}
+
+
+
+
