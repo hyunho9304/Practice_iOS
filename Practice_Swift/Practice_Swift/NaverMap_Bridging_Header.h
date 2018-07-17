844e0dd994fa5deea19d73b19e9c6e9c1256a3e8
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-07-17 18:34:19 +0900

    COMMIT // NaverMap API setup

---
+++ Practice_Swift/Practice_Swift/NaverMap_Bridging_Header.h
@@ -0,0 +1,15 @@
+//
+//  NaverMap_Bridging_Header.h
+//  Practice_Swift
+//
+//  Created by 박현호 on 2018. 7. 17..
+//  Copyright © 2018년 박현호. All rights reserved.
+//
+
+#ifndef NaverMap_Bridging_Header_h
+#define NaverMap_Bridging_Header_h
+
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+#endif /* NaverMap_Bridging_Header_h */
